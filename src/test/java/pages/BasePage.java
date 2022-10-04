0eaf4f63e492c798868b5423ed24a84f3c8cb9d2
rangalo83 <goranpincir@gmail.com>
2022-10-04 18:45:31 +0200

    "Commit pom and BasePage"

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    private WebDriver driver;
+    private WebDriverWait driverWait;
+
+    public BasePage(WebDriver driver, WebDriverWait driverWait) {
+        this.driver = driver;
+        this.driverWait = driverWait;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getDriverWait() {
+        return driverWait;
+    }
+}
