dbb04c04a32e279561ad756a646fd079c6619525
rangalo83 <goranpincir@gmail.com>
2022-10-09 13:25:44 +0200

    "Commiting visit TheSignUpPageTest "

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,8 +7,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage {
 
-    protected By logIn = By.xpath("//*[@id=\"app\"]/div/div/header/div/div[3]/a[3]");
-
+    protected By logIn = By.xpath("//*[@id='app']/div/div/header/div/div[3]/a[3]");
+    protected By signUp = By.xpath("//*[@id='app']/div/div/header/div/div[3]/a[4]");
     public HomePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
@@ -16,9 +16,17 @@ public class HomePage extends BasePage {
     public WebElement getLogIn() {
         return getDriver().findElement(logIn);
     }
+    public WebElement getSignUp() {
+        return getDriver().findElement(signUp);
+    }
 
     public void logInButton() {
         WebElement logInButton = getLogIn();
         logInButton.click();
+    }
+
+    public void signUpButton() {
+        WebElement signUpButton = getSignUp();
+        signUpButton.click();
     }
 }
---
+++ src/test/java/tests/signUpTests/VisitsTheSignupPage.java
@@ -0,0 +1,22 @@
+package tests.signUpTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class VisitsTheSignupPage extends BaseTest {
+    //assert:
+    //Verifikovati da se u url-u stranice javlja /signup ruta
+
+    @Test
+
+    public void visitTheSignUpPageTest() throws InterruptedException {
+    homePage.signUpButton();
+        Thread.sleep(3000);
+        String expectedResult = "/signup";
+        String signUpPageUrl = driver.getCurrentUrl();
+        Assert.assertTrue(signUpPageUrl.contains(expectedResult));
+    }
+
+
+}
